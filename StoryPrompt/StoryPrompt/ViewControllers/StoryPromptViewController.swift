361fcdeff52326265a9e4011da97fb6cb972d6b5
Sara Felix <70541669+sarafelix25@users.noreply.github.com>
2022-05-02 12:15:04 -0300

    UIKit Fundamentals
    Learn UIKit Controls and Add Another Screen

---
+++ StoryPrompt/StoryPrompt/ViewControllers/StoryPromptViewController.swift
@@ -0,0 +1,42 @@
+//
+//  StoryPromptViewController.swift
+//  StoryPrompt
+//
+//  Created by Sara Batista dos Santos Felix on 29/04/22.
+//
+
+import UIKit
+
+class StoryPromptViewController: UIViewController {
+
+    @IBOutlet weak var storyPromptTextView: UITextView!
+    
+    var storyPrompt: StoryPromptEntry?
+    var isNewStoryPrompt = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        storyPromptTextView.text = storyPrompt?.description
+        if isNewStoryPrompt {
+            let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveStoryPrompt))
+            let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelStoryPrompt))
+            navigationItem.rightBarButtonItem = saveButton
+            navigationItem.leftBarButtonItem = cancelButton
+        }
+    }
+    
+    
+    @objc func cancelStoryPrompt(_ sender: Any) {
+        performSegue(withIdentifier: "CancelStoryPrompt", sender: nil)
+    }
+    
+    @objc func saveStoryPrompt(_ sender: Any) {
+        NotificationCenter.default.post(name: .StoryPromptSaved, object: storyPrompt)
+        performSegue(withIdentifier: "SaveStoryPrompt", sender: nil)
+    }
+}
+
+extension Notification.Name {
+    static let StoryPromptSaved = Notification.Name("StoryPromptSave")
+}
